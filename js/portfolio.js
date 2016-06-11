e308aa11ada06d46004e1e06272a4effc6bb789f
wy19951231 <1575456841@qq.com>
2016-06-11 18:57:55 +0800

    add main.css

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,7 +5,9 @@
 			height: 193,
 			marginLeft: -10,
 			marginTop: -7
-		});
+		});//在li上面设置hover事件是因为如果事件在img上设置事件
+		//则会导致划入img，mask显示，显示之后就划入mask，则事件
+		//结束，隐藏mask，再划入img，造成一种反复过程
 		$(this).children('.mask').stop().show().animate({
 			opacity: 0.84
 		});
@@ -19,63 +21,38 @@
 		$(this).children('.mask').stop().animate({
 			opacity: 0
 		});
+	}).on('click',function(){
+		console.log($(this).data('src'));//取img
 	});
 
-	window.onload=function(){
-		//var oBtn1 = document.getElementById('portfolio');
-		//var oBtn = oBtn1.getElementsByTagName('li');
-		$('#portfolio li').click(function(){
-			var sHeight = document.documentElement.scrollHeight;//获取页面高度
-			var sWidth = document.documentElement.scrollWidth;
-
-			var wHeight = document.documentElement.clientHeight;//获取可视区域的高度
-			//如果页面是一个竖向的页面，可视区域的宽度和页面的宽度是一样的
-			//alert(wHeight);
-			// var oPort = document.getElementById('#portfolio');
-			// var oPic = oPort.getElementsByTagName('li');
-
-			var oMask1 = document.createElement('div');//创建节点
-				oMask1.id = "mask1";
-				oMask1.style.height = sHeight+"px";
-				oMask1.style.width = sWidth+"px";
-				document.body.appendChild(oMask1);//插入节点
-
-			var oMask2 = document.createElement('div');
-				oMask2.id="mask2";
-				oMask2.innerHTML = "<div class='pic'><div id='close'></div></div>";
-				document.body.appendChild(oMask2);
-			// var	oPic = oMask2.getElementsByClassName('pic');
-			// 	oPic[0].style.backgroundImage = "url(images/port-pic.jpg)";
-
-			//怎样插入图片
-			var dHeight = oMask2.offsetHeight;//获取弹层的高度
-			var dWidth = oMask2.offsetWidth;
-				oMask2.style.height = (wHeight-dHeight)/2+"px";
-				oMask2.style.left = (sWidth-dWidth)/3*2+"px";
-			var oClose = document.getElementById("close");
-				oMask1.onclick = oClose.onclick = function(){
-					document.body.removeChild(oMask1);
-					document.body.removeChild(oMask2);
-				}
+	$('#portfolio li').click(function(){
+		var iWidth = this.getAttribute('data-width')>=600?600:this.getAttribute('data-width');
+		var iHeight = this.getAttribute('data-height')*((this.getAttribute('data-width'))/iWidth);
+		$mask = $('<div class="mask"></div>').appendTo(document.body);
+		$close = $('<div class="cl"></div>').css({
+			width:13,
+			height:13,
+			left:($(window).width() - iWidth)/2,
+			top:($(window).height() - iHeight)/2
+		}).appendTo(document.body);
+		$div = $('<div class="lightbox-img"><div class="loading">loading</div></div>').css({
+			width:iWidth,
+			height:this.getAttribute('data-height'),
+			left:($(window).width() - iWidth)/2,
+			top:($(window).height() - iHeight)/2
+		}).appendTo(document.body);
+		var oImg = new Image();
+		oImg.onload = function(){
+			$div.children('.loading').hide();
+			this.width = iWidth;
+			$div.append(oImg);
+		};
+		oImg.src = this.getAttribute('data-src');
+		$(".mask").click(function(){
+			$mask.hide();
+			$div.hide();
+			$close.hide();
 		});
-	}
-
-	/*$('#portfolio').click(function(){
-		$(this).children('.wrap').animate({
-			opacity: 1
 		})
-	});*/
-
-	/*$('#portfolio li').on('click', function(){
-		lb.open({
-			src: $(this).data('src'),
-			width: $(this).data('width'),
-			height: $(this).data('height')
-		});
 	});
 
-	$('#btn').on('click', function(){
-		$(this).animate({
-			backgroundColor: 'rgb(0,0,255)'
-		});
-	});*/
